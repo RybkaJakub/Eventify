7c04d82121b69cc685977725b5524010f49d199f
RybkaJakub <jakubekrybka@gmail.com>
2024-05-14 11:14:23 +0200

    01

--- eventify_app/views.py
+++ eventify_app/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html', context={})
+    user = request.user
+    divider_rendered = False
+    for group in user.groups.all():
+        if group.name == "eventer" or group.name == "admin":
+            divider_rendered = True
+            break
+
+    context = {
+        'divider_rendered': divider_rendered,
+    }
+    return render(request, 'index.html', context=context)
